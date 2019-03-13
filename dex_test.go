5962c67e064b6bb1f87bb3094dd94449c0b1f018
cdyfng <xiaofeng_chen2007@163.com>
2019-03-13 23:24:13 +0800

    binance chain function test

---
+++ dex_test.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	time2 "time"
+
+	"github.com/stretchr/testify/assert"
+
+	sdk "github.com/binance-chain/go-sdk/client"
+	"github.com/binance-chain/go-sdk/client/query"
+	//"github.com/binance-chain/go-sdk/common"
+	"github.com/binance-chain/go-sdk/keys"
+	"github.com/binance-chain/go-sdk/types"
+	"github.com/binance-chain/go-sdk/types/msg"
+	//tx2 "github.com/binance-chain/go-sdk/types/tx"
+)
+
+// After bnbchain integration_test.sh has runned
+func TestAllProcess(t *testing.T) {
+	//----- Recover account ---------
+	// mnemonic := "test mnemonic"
+	// keyManager, err := keys.NewMnemonicKeyManager(mnemonic)
+	priv := ""
+	priv2 := ""
+	keyManager, err := keys.NewPrivateKeyManager(priv)
+	assert.NoError(t, err)
+	testAccount1 := keyManager.GetAddr()
+	testKeyManager2, _ := keys.NewPrivateKeyManager(priv2)//keys.NewKeyManager()
+	testAccount2 := testKeyManager2.GetAddr()
+	fmt.Printf("testAccount1: %v \n", testAccount1)
+	fmt.Printf("testAccount2: %v \n", testAccount2)
+
+	//-----   Init sdk  -------------
+	client, err := sdk.NewDexClient("https://testnet-dex.binance.org", types.TestNetwork, keyManager)
+	assert.NoError(t, err)
+	nativeSymbol := msg.NativeToken
+
+	// //-----  Get account  -----------
+	// account, err := client.GetAccount(testAccount1.String())
+	// //fmt.Printf("account: %v \n", account)
+	// assert.NoError(t, err)
+	// assert.NotNil(t, account)
+	// assert.True(t, len(account.Balances) > 0)
+
+	//----- Get Markets  ------------
+	// markets, err := client.GetMarkets(query.NewMarketsQuery().WithLimit(1000))
+	// assert.NoError(t, err)
+	// fmt.Printf("markets Num: %v \n", len(markets))
+	//fmt.Printf("markets: %v \n", markets)
+	//tradeSymbol := markets[0].TradeAsset
+	// for _, m := range markets {
+	// 	fmt.Printf("%v \n", m)
+	// 	// if m.QuoteAsset == "BTC.B-918" {
+	// 	//  	tradeSymbol = m.TradeAsset
+	// 	//  	fmt.Printf("       %v \n", tradeSymbol)
+	// 	// }		
+	// }
+	tradeSymbol := "BNB" 
+	nativeSymbol = "BTC.B-918"
+	//assert.Equal(t, 1, len(markets))
+	
+	// if markets[0].QuoteAsset != "BNB" {
+	// 	tradeSymbol = markets[0].QuoteAsset
+	// }
+
+	//-----  Get Depth  ----------
+	dq := query.NewDepthQuery(tradeSymbol, nativeSymbol)
+	fmt.Printf("Depth Query: %v \n", dq)
+	depth, err := client.GetDepth(dq)
+	assert.NoError(t, err)
+	fmt.Printf("depth: %v \n", depth) 
+	assert.True(t, depth.Height > 0)
+	fmt.Printf("tradeSymbol: %v \n", tradeSymbol)
+	fmt.Printf("nativeSymbol: %v \n", nativeSymbol)
+
+	//----- Get Kline
+	kline, err := client.GetKlines(query.NewKlineQuery(tradeSymbol, nativeSymbol, "1h").WithLimit(1))
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(kline))
+
+	//-----  Get Ticker 24h  -----------
+	ticker24h, err := client.GetTicker24h(query.NewTicker24hQuery().WithSymbol(tradeSymbol, nativeSymbol))
+	assert.NoError(t, err)
+	assert.True(t, len(ticker24h) > 0)
+
+	//-----  Get Tokens  -----------
+	tokens, err := client.GetTokens()
+	assert.NoError(t, err)
+	fmt.Printf("GetTokens: %v \n", len(tokens))
+
+	//-----  Get Trades  -----------
+	fmt.Println(testAccount1.String())
+	trades, err := client.GetTrades(query.NewTradesQuery(testAccount1.String(), true).WithSymbol(tradeSymbol, nativeSymbol))
+	assert.NoError(t, err)
+	fmt.Printf("GetTrades: %v \n", trades)
+
+	//-----  Get Time    -----------
+	time, err := client.GetTime()
+	assert.NoError(t, err)
+	fmt.Printf("Get time: %v \n", time)
+
+	//----- Create order -----------
+	//createOrderResult, err := client.CreateOrder(tradeSymbol, nativeSymbol, msg.OrderSide.BUY, 30000000000, 10000000000000, true)
+	createOrderResult, err := client.CreateOrder(tradeSymbol, nativeSymbol, msg.OrderSide.BUY, 39385300, 100000000, true)
+	assert.NoError(t, err)
+	assert.True(t, true, createOrderResult.Ok)
+
+	//---- Get Open Order ---------
+	openOrders, err := client.GetOpenOrders(query.NewOpenOrdersQuery(testAccount1.String(), true))
+	assert.NoError(t, err)
+	assert.True(t, len(openOrders.Order) > 0)
+	orderId := openOrders.Order[0].ID
+	fmt.Printf("GetOpenOrders:  %v \n", openOrders)
+	fmt.Printf("orderId:  %v \n", orderId)
+	
+
+	// //---- Get Order    ------------
+	// order, err := client.GetOrder(orderId)
+	// assert.NoError(t, err)
+	// assert.Equal(t, common.CombineSymbol(tradeSymbol, nativeSymbol), order.Symbol)
+
+	// //---- Cancle Order  ---------
+	 time2.Sleep(2 * time2.Second)
+	// cancleOrderResult, err := client.CancelOrder(tradeSymbol, nativeSymbol, orderId, true)
+	// assert.NoError(t, err)
+	// assert.True(t, cancleOrderResult.Ok)
+	// fmt.Printf("cancleOrderResult:  %v \n", cancleOrderResult)
+
+	// //---- Get Close Order---------
+	// closedOrders, err := client.GetClosedOrders(query.NewClosedOrdersQuery(testAccount1.String(), true).WithSymbol(tradeSymbol, nativeSymbol))
+	// assert.NoError(t, err)
+	// assert.True(t, len(closedOrders.Order) > 0)
+	// fmt.Printf("GetClosedOrders: %v \n", closedOrders)
+
+	// //----    Get tx      ---------
+	// tx, err := client.GetTx(openOrders.Order[0].TransactionHash)
+	// assert.NoError(t, err)
+	// assert.NotNil(t, tx)
+	// fmt.Printf("GetTx: %v\n", tx)
+
+	// //----   Send tx  -----------
+	// send, err := client.SendToken(testAccount2, nativeSymbol, 10000000000, true)
+	// assert.NoError(t, err)
+	// assert.True(t, send.Ok)
+	// fmt.Printf("Send token: %v\n", send)
+
+	// //---    Get test2 account-----
+	// newTestAccout2, err := client.GetAccount(testAccount2.String())
+	// assert.NoError(t, err)
+	// for _, c := range newTestAccout2.Balances {
+	// 	if c.Symbol == nativeSymbol {
+	// 		fmt.Printf("test account BNB: %s \n", c.Free)
+	// 	}
+	// }
+
+	// //----   Freeze Token ---------
+	// freeze, err := client.FreezeToken(nativeSymbol, 100000000, true)
+	// assert.NoError(t, err)
+	// assert.True(t, freeze.Ok)
+	// fmt.Printf("freeze token: %v\n", freeze)
+
+	// //----   Unfreeze Token ---------
+	// unfreeze, err := client.UnfreezeToken(nativeSymbol, 100000000, true)
+	// assert.NoError(t, err)
+	// assert.True(t, unfreeze.Ok)
+	// fmt.Printf("Unfreeze token: %v\n", unfreeze)
+
+	// //----   issue token ---------
+	// issue, err := client.IssueToken("Client-Token", "sdk", 10000000000000000, true, true)
+	// assert.NoError(t, err)
+	// fmt.Printf("Issue token: %v\n", issue)
+
+	// //---  check issue success ---
+	// time2.Sleep(2 * time2.Second)
+	// issueresult, err := client.GetTx(issue.Hash)
+	// assert.NoError(t, err)
+	// assert.True(t, issueresult.Code == tx2.CodeOk)
+
+	// //--- mint token -----------
+	// mint, err := client.MintToken(issue.Symbol, 100000000, true)
+	// assert.NoError(t, err)
+	// fmt.Printf("Mint token: %v\n", mint)
+
+	// //---- Submit Proposal ------
+	// time2.Sleep(2 * time2.Second)
+	// listTradingProposal, err := client.SubmitListPairProposal("New trading pair", msg.ListTradingPairParams{issue.Symbol, nativeSymbol, 1000000000, "my trade", time2.Now().Add(1 * time2.Hour)}, 200000000000, true)
+	// assert.NoError(t, err)
+	// fmt.Printf("Submit list trading pair: %v\n", listTradingProposal)
+
+	// //---  check submit proposal success ---
+	// time2.Sleep(2 * time2.Second)
+	// submitPorposalStatus, err := client.GetTx(listTradingProposal.Hash)
+	// assert.NoError(t, err)
+	// assert.True(t, submitPorposalStatus.Code == tx2.CodeOk)
+
+	// //---- Vote Proposal  -------
+	// time2.Sleep(2 * time2.Second)
+	// vote, err := client.VoteProposal(listTradingProposal.ProposalId, msg.OptionYes, true)
+	// assert.NoError(t, err)
+	// fmt.Printf("Vote: %v\n", vote)
+
+	// //--- List trade pair ------
+	// l, err := client.ListPair(listTradingProposal.ProposalId, issue.Symbol, nativeSymbol, 1000000000, true)
+	// assert.NoError(t, err)
+	// fmt.Printf("List trading pair: %v\n", l)
+	// //---- Get new markets
+	// //time2.Sleep(1 * time2.Minute)
+	// //markets, err = client.GetMarkets(&api.MarketsQuery{Limit: 1, Offset: 0})
+	// //assert.NoError(t, err)
+	// //fmt.Printf("New markets: %v \n ", markets)
+
+}
